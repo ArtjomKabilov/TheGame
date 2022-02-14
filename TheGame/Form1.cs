47d3977255bd970bf14222ee3bcd943cd8fad52a
Artem Kabilov <artem1223148@gmail.com>
2022-02-14 22:40:45 +0300

    Добавьте файлы проекта.

---
+++ TheGame/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheGame
+{
+    public partial class Form1 : Form
+    {
+        Rectangle Rectangle = new Rectangle(10, 10, 200, 100);
+        Rectangle Circle = new Rectangle(220, 10, 150, 150);
+        Rectangle Square = new Rectangle(380, 10, 150, 150);
+        public bool RectangleClicked = false;
+        bool CircleClicked = false;
+        bool SquareClicked = false;
+        int RectangleX = 0;
+        int RectangleY = 0;
+        int CircleX = 0;
+        int CircleY = 0;
+        int SquareX = 0;
+        int SquareY = 0;
+        int X, Y, dX, dY;
+        int LastClicked = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillEllipse(Brushes.Red, Circle);
+            e.Graphics.FillRectangle(Brushes.Blue, Square);
+            e.Graphics.FillRectangle(Brushes.Yellow, Rectangle);
+        }
+
+        private void up2(object sender, MouseEventArgs e)
+        {
+            if (LastClicked == 2)
+            {
+                if ((label2.Location.X < Square.X + Square.Width) && (label2.Location.X > Square.X))
+                {
+                    if ((label2.Location.Y < Square.Y + Square.Height) && (label2.Location.Y > Square.Y))
+                    {
+                        X = Circle.X;
+                        Y = Circle.Y;
+                        dX = CircleX;
+                        dY = CircleY;
+                        Circle.X = Square.X;
+                        Circle.Y = Square.Y;
+                        CircleX = SquareX;
+                        CircleY = SquareY;
+
+                        Square.X = X;
+                        Square.Y = Y;
+                        SquareX = dX;
+                        SquareY = dY;
+                    }
+
+                }
+            }   
+        }
+
+        private void name(object sender, MouseEventArgs e)
+        {
+            if ((e.X < Circle.X + Circle.Width) && (e.X > Circle.X))
+            {
+                if ((e.Y < Circle.Y + Circle.Height) && (e.Y > Circle.Y))
+                {
+                    CircleClicked = true;
+
+                    CircleX = e.X - Circle.X;
+                    CircleY = e.Y - Circle.Y;
+                }
+            }
+            if ((e.X < Rectangle.X + Rectangle.Width) && (e.X > Rectangle.X))
+            {
+                if ((e.Y < Rectangle.Y + Rectangle.Height) && (e.Y > Rectangle.Y))
+                {
+                    RectangleClicked = true;
+
+                    RectangleX = e.X - Rectangle.X;
+                    RectangleY = e.Y - Rectangle.Y;
+                }
+            }
+            if ((e.X < Square.X + Square.Width) && (e.X > Square.X))
+            {
+                if ((e.Y < Square.Y + Square.Height) && (e.Y > Square.Y))
+                {
+                    SquareClicked = true;
+
+                    SquareX = e.X - Square.X;
+                    SquareY = e.Y - Square.Y;
+                }
+            }
+
+        }
+
+        private void up(object sender, MouseEventArgs e)
+        {
+            if (CircleClicked)
+            {
+                Circle.X = e.X - CircleX;
+                Circle.Y = e.Y - CircleY;
+                pictureBox1.Invalidate();
+            }
+            if (SquareClicked)
+            {
+                Square.X = e.X - SquareX;
+                Square.Y = e.Y - SquareY;
+                pictureBox1.Invalidate();
+            }
+            if (RectangleClicked)
+            {
+                Rectangle.X = e.X - RectangleX;
+                Rectangle.Y = e.Y - RectangleY;
+                pictureBox1.Invalidate();
+            }
+
+        }
+
+        private void move(object sender, MouseEventArgs e)
+        {
+            if ((label1.Location.X < Square.X + Square.Width) && (label1.Location.X > Square.X))
+            {
+                if ((label1.Location.Y < Square.Y + Square.Height) && (label1.Location.Y > Square.Y))
+                {
+                    label3.Text = "Синий квадрат";
+                }
+            }
+            if ((label1.Location.X < Rectangle.X + Rectangle.Width) && (label1.Location.X > Rectangle.X))
+            {
+                if ((label1.Location.Y < Rectangle.Y + Rectangle.Height) && (label1.Location.Y > Rectangle.Y))
+                {
+                    label3.Text = "R квадрат";
+                }
+            }
+            if ((label1.Location.X < Circle.X + Circle.Width) && (label1.Location.X > Circle.X))
+            {
+                if ((label1.Location.Y < Circle.Y + Circle.Height) && (label1.Location.Y > Circle.Y))
+                {
+                    label3.Text = "F квадрат";
+                }
+            }
+        }
+    }
+}
